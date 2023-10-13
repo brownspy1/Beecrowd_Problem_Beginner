cfc58ae786576e62d0b78ec114e585b30095c87d
M.Mahadi <motivation1424@gmail.com>
2023-10-14 02:13:43 +0600

    Update faltu.py

--- Begaining problem/faltu.py
+++ Begaining problem/faltu.py
@@ -1,10 +1,21 @@
-"""October
-Ctober
-Tober
-Ober
-Ber
-Er
-R🥰🥰"""
-munth = 'February'
-for i in range(len(munth) -1, -1, -1):
-    print(munth[i:])
+# print গুণিতক using for loop
+n = int(input())
+gunitok = []
+for i in range(1, n + 1):
+    if n % i == 0:
+        gunitok.append(i)
+print(f'The factors of the {n} are:{gunitok}')
+
+# printing gunoniok using looping
+n = int(input())
+hi = []
+num = []
+for x in range(n):
+    ver = n
+    num.append(ver)
+    if x >= 1:
+        break
+    for j in range(1, n + 1):
+        sonkha = num[x] * j
+        hi.append(sonkha)
+print(f'{n} ar gunitok : {hi}')
